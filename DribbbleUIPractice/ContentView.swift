1a53727e6a4ae59b9a586916066007ac233c5434
Dean Thompson <deanthompson.music@gmail.com>
2022-06-18 14:13:43 +0900

    Initial Commit

---
+++ DribbbleUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DribbbleUIPractice
+//
+//  Created by Dean Thompson on 2022/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DribbbleUIPractice/DribbbleUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbbleUIPracticeApp.swift
+//  DribbbleUIPractice
+//
+//  Created by Dean Thompson on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct DribbbleUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
